ce3320e265d75ff8107366d7b36c17eb843b5e2b
yezerpo <kalidium@outlook.com>
2025-02-12 21:38:30 +0800

    update

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -168,7 +168,7 @@ export default hopeTheme({
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
     comment: {
        provider: "Artalk",
-       server: "https://arktalk.yezer.cn",
+       server: "https://artalk.yezer.cn",
      },
 
     components: {
